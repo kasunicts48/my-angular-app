4ea71fcf69f9069c9588250a5b322f134f7488a3
Kasun Dananjaya <kasundananjayaweerasooriya@gmail.com>
2024-08-10 17:02:05 +0530

    Angular Part 04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  title: string = 'This loaded dynamically';
+  imgURL: string =
+    'https://images.unsplash.com/photo-1715062716491-98bf1d9e59dc';
+  isDisable: boolean = false;
+  isActive: boolean = false;
 }
